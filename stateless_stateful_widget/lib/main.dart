b3225854972cb46bd4c990b033ae06c94aeb69cd
fizabells22 <fizabella.ru@gmail.com>
2022-02-28 18:15:23 +0700

    Konverter Suhu Flutter

--- stateless_stateful_widget/lib/main.dart
+++ stateless_stateful_widget/lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
   void konversisuhu(){
     setState((){
     _inputUser = double.parse(etInput.text);
-    _kelvin = _inputUser + 273.15;
+    _kelvin = _inputUser + 273;
     _reamur = _inputUser * 0.8;
   });
   }
@@ -36,54 +36,59 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-        title: Text ("Konverter Suhu"),
+        title: const Text ("Konverter Suhu"),
         ),
         body: Container(
-          margin: EdgeInsets.all(8),
+          margin: const EdgeInsets.all(8),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               TextFormField(
-                decoration: InputDecoration( hintText: "Masukkan Suhu Dalam Celcius"),
-                inputFormatters : [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration( hintText: "Masukkan Suhu Dalam Celcius"),
                 controller: etInput,
                 keyboardType: TextInputType.number,
               ),
-            ],
-            //bikin row untuk text suhu dalam kelvin dan reamur
+            Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        const Text("Suhu Dalam Kelvin", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200,
+                          ),
+                        ),
+                        Text("$_kelvin", style: const TextStyle( fontSize: 40 , fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text("Suhu Dalam Reamur", style: TextStyle(fontSize: 18,fontWeight: FontWeight.w200,
+                          ),
+                        ),
+                        Text("$_reamur", style: const TextStyle(fontSize: 40, fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+          Container(
+          width: double.infinity,
+          height : 50,
           child : RaisedButton(
-            textColor: Colors.white,
-            onPressed: (){},
-            color : Colors.blue,
-            alignment : Alignment.bottomCenter,
+          textColor: Colors.white,
+          onPressed: konversisuhu,
+          color : Colors.blue,
             child : const Text(
               "Konversi Suhu",
               style: TextStyle(fontSize: 20),
             ),
           ),
           ),
-        ),
+            ],
+        ),),
       ),
     );
   }
 }
-
-Convert (konvertHandler: _konversiSuhu),
-
-class Convert extends StatelessWidget{
-  final Function konvertHandler;
-
-  Convert({Key key, @required this.konvertHandler});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 50,
-      child: RaisedButton(
-        onPressed: konvertHandler,
-        color: Colors.blueAccent,
-        textColor: Colors.white,
-        child: Text("Konversi Suhu"),
-        ),
-    );
-  }
-  }
